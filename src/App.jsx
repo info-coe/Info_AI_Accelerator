94d25cdef24b6ed3d43aaa1984cdf41e3c0d15de
info-coe <coeteam@infomericainc.in>
2024-08-19 16:08:01 +0530

    updated design

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,18 @@
-// import "./App.css";
-// import InfoAIAccelerator from "./InfoAIAccelerator";
-// import Register from "./Register";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-// function App() {
-//   return (
-//     <>
-//       <BrowserRouter basename="Info_AI_Accelerator">
-//         <Routes>
-//           <Route path="/" element={<Register />}/>
-//           <Route path="main" element={<InfoAIAccelerator/>}/>
-//         </Routes>
-//       </BrowserRouter>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import RegisterModal from "./Register";
 import "bootstrap/dist/css/bootstrap.min.css";
 import InfoAIAccelerator from "./InfoAIAccelerator";
-
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import "./App.css"
 
 const App = () => {
   return (
     <div className="App">
-      {/* <h1>Welcome to Our Website</h1> */}
+      <Navbar/>
       <InfoAIAccelerator/>
-      {/* <RegisterModal /> */}
+      <Footer/>
       <RegisterModal/>
-      {/* Rest of your website content */}
     </div>
   );
 };
--- src/InfoAIAccelerator.jsx
+++ src/InfoAIAccelerator.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import AI1 from "./Images/AI1.jpg";
 import AI2 from "./Images/AI2.jpg";
 import AI3 from "./Images/AI3.jpg";
-import AIBanner from "./Images/AIBanner.jpg"
+import AIBanner from "./Images/AIBanner.jpg";
 
 export default function InfoAIAccelerator() {
   const infoAIAcceleratorFeatures = [
@@ -60,92 +60,75 @@ export default function InfoAIAccelerator() {
   ];
 
   return (
-    <div className="container">
-      <div className="shadow rounded p-3 mt-5 mb-3">
-        <h1 className="text-center">Info AI Accelerator</h1>
-        <div className="row align-items-center">
-        <div className="col-md-9">
-          <p>
-            Info AI Accelerator is a revolutionary platform that simplifies the
-            training, building, and testing of Large Language Models (LLMs).
-            Traditional LLM development is often hampered by high resource
-            demands and time-consuming processes. Info AI Accelerator addresses
-            these challenges by offering a user-friendly interface that
-            significantly reduces GPU usage and accelerates model training with
-            advanced multithreading technology. This tool allows you to focus on
-            innovation and fine-tuning while we take care of the technical
-            complexities.
-          </p>
-          <p>
-            Info AI Accelerator is ideal for data scientists, AI researchers,
-            and developers who need a robust, efficient solution to streamline
-            their LLM workflows.
-          </p>
-        </div>
-        <div className="text-center col-md-3" style={{  }}>
-          <img src={AIBanner} alt="Services" width="300" height="300" />
-        </div>
-        </div>
-       
+    <>
+      <div className="awsmain">
+        <img
+          src="https://www.infomericainc.com/Content/images/services-banner.jpg"
+          alt="Aws Services"
+          width="100%"
+        />
       </div>
-      <div className="mt-5">
-        <h2>Features</h2>
-        {/* <div className="grid-container">
-          {infoAIAcceleratorFeatures.map((item, index) => (
-            <div key={index} className="card border m-2 p-3">
-              <h4 className="text-center">{item.title}</h4>
-              <p>{item.description}</p>
+      <div className="">
+        <div className="mt-5 mb-3 pe-5 ps-5">
+          <h1 className="text-center">Info AI Accelerator</h1>
+          <div className="d-md-flex gap-4 align-items-center">
+            <div className="text-center col-md-3" style={{}}>
+              <img src={AIBanner} alt="Services" width="100%"/>
             </div>
-          ))}
-        </div> */}
-    <div className="container p-md-5 p-2">
-  <div className="row justify-content-center">
-    {infoAIAcceleratorFeatures.map((item, index) => (
-      <div key={index} className="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center mb-4 p-2">
-        <div className="card custom-card border p-3">
-          <h4 className="text-center">{item.title}</h4>
-          <p>{item.description}</p>
-        </div>
-      </div>
-    ))}
-  </div>
-</div>
-
-
-
-
-
-
-      </div>
-      <div>
-        <h2>Our Services</h2>
-        {/* <div className="grid-container">
-          {infoAIAcceleratorServices.map((item, index) => (
-            <div key={index} className="card shadow m-2 p-3">
-              <div className="" style={{  }}>
-                <img src={item.image} alt="Services" width="300" height="300" />
-              </div>
-              <h4 style={{ }}>{item.title}</h4>
-              <p>{item.description}</p>
+            <div className="col-md-9">
+              <p>
+                Info AI Accelerator is a revolutionary platform that simplifies
+                the training, building, and testing of Large Language Models
+                (LLMs). Traditional LLM development is often hampered by high
+                resource demands and time-consuming processes. Info AI
+                Accelerator addresses these challenges by offering a
+                user-friendly interface that significantly reduces GPU usage and
+                accelerates model training with advanced multithreading
+                technology. This tool allows you to focus on innovation and
+                fine-tuning while we take care of the technical complexities.
+              </p>
+              <p>
+                Info AI Accelerator is ideal for data scientists, AI
+                researchers, and developers who need a robust, efficient
+                solution to streamline their LLM workflows.
+              </p>
             </div>
-          ))}
-        </div> */}
-        <div className="container p-md-5 p-2">
-  <div className="row justify-content-center">
-    {infoAIAcceleratorServices.map((item, index) => (
-      <div key={index} className="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center mb-4">
-        <div className="card custom-card border p-3">
-        <div className="text-center" style={{  }}>
-                <img src={item.image} alt="Services" width="300" height="300" />
+          </div>
+        </div>
+
+        <div className="container pt-4">
+          <h2 className="section-header">Features</h2>
+          <div className="grid-container">
+            {infoAIAcceleratorFeatures.map((item, index) => (
+              <div className=" m-2 grid-item" key={index}>
+                <div className="features-box">
+                  <div>
+                    <h4 className="features-title">
+                      {item.title}
+                    </h4>
+                    <p className="features-description">{item.description}</p>
+                  </div>
+                </div>
               </div>
-          <h4 className="text-center">{item.title}</h4>
-          <p>{item.description}</p>
+            ))}
+          </div>
+        </div>
+
+        <div className="container mb-4">
+          <h2 className="section-header">Our Services</h2>
+          <div className="grid-container">
+            {infoAIAcceleratorServices.map((item, index) => (
+              <div key={index} className="grid-item shadow m-2 p-3">
+                <div className="" style={{ height: "54%" }}>
+                  <img src={item.image} alt="Services" width="70%" />
+                </div>
+                <h4 style={{ height: "17%" }}>{item.title}</h4>
+                <p>{item.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    ))}
-  </div>
-</div>
-      </div>
-    </div>
+    </>
   );
 }
